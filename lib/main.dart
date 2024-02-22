4b9cb49bd18006eb42d20478df27e42f3c6a3964
yash chotaliya <yashchotaliya187@gmail.com>
2024-02-22 17:18:29 +0530

    " init "

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Hack The Spring'),
     );
   }
 }
